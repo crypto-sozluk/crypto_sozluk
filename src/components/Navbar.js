fbe8e2b92107f8ef74db4a884d37a09592780301
deeppaz <deeppaz@hotmail.com>
2019-11-09 23:48:13 +0300

    added navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Link from 'react-router-dom/Link';
+
+// material-ui islevleri
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
+
+class Navbar extends Component {
+    render() {
+        return (
+            <AppBar>
+                <Toolbar className="nav-container">
+                    <Button color="inherit" component={Link} to="/giris">Giris</Button>
+                    <Button color="inherit" component={Link} to="/">Anasayfa</Button>
+                    <Button color="inherit" component={Link} to="/uyeol">Uye ol</Button>
+                </Toolbar>
+            </AppBar>
+        );
+    }
+}
+
+export default Navbar
