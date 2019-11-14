d5e29f5c25cc86dc9bf8f79f9c4b87014ed33e1c
deeppaz <deeppaz@hotmail.com>
2019-11-14 23:52:20 +0300

    added upload image

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,14 +9,18 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import MuiLink from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
+import IconButton  from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
 
 //icons 
 import LocationOn from '@material-ui/icons/LocationOn';
 import LinkIcon from '@material-ui/icons/Link';
 import CalendarToday from '@material-ui/icons/CalendarToday';
+import EditIcon from '@material-ui/icons/Edit';
 
 //redux
 import { connect } from 'react-redux';
+import { logoutUser, uploadImage } from '../redux/actions/userActions'
 
 
 const styles = (theme) => ({
@@ -68,6 +72,16 @@ const styles = (theme) => ({
 });
 
 export class Profile extends Component {
+    handleImageChange = (event) => {
+        const image = event.target.files[0];
+        const formData = new FormData();
+        formData.append('image', image, image.name);
+        this.props.uploadImage(formData);
+    };
+    handleEditPicture = () =>{
+        const fileInput = document.getElementById('imageInput');
+        fileInput.click();
+    };
     render() {
         const { classes, user: { credentials: { handle, createdAt, imageUrl, bio, website, location }, loading, authenticated } } = this.props;
         let profileMarkup = !loading ? (authenticated ? (
@@ -75,6 +89,12 @@ export class Profile extends Component {
                 <div className={classes.profile}>
                     <div className="image-wrapper">
                         <img src={imageUrl} alt="profile" className="profile-image" />
+                        <input type="file" id="imageInput" hidden="hidden" onChange={this.handleImageChange}/>
+                        <Tooltip title="edit profile" placement="top">
+                        <IconButton onClick={this.handleEditPicture} className="button">
+                            <EditIcon color="primary"/>
+                        </IconButton>
+                        </Tooltip>
                     </div>
                     <hr />
                     <div className="profile-details">
@@ -127,9 +147,13 @@ const mapStateToProps = (state) => ({
     user: state.user
 });
 
+const mapActionsToProps = { logoutUser, uploadImage };
+
 Profile.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    uploadImage: PropTypes.func.isRequired,
     user: PropTypes.object.isRequired,
     classes: PropTypes.object.isRequired,
-}
+};
 
-export default connect(mapStateToProps)(withStyles(styles)(Profile))
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Profile))
