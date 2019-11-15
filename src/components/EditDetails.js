59e7198aa521e55009d5ff16eaf212bf4e7666bb
deeppaz <deeppaz@hotmail.com>
2019-11-15 23:52:51 +0300

    added navbar buttons

--- src/components/EditDetails.js
+++ src/components/EditDetails.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
+import CSButton from '../util/CSButton';
 
 //redux
 import { connect } from 'react-redux';
@@ -13,8 +14,6 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Tooltip from '@material-ui/core/Tooltip';
-import IconButton from '@material-ui/core/IconButton';
 import EditIcon from '@material-ui/icons/Edit';
 
 const styles = (theme) => ({
@@ -74,11 +73,9 @@ class EditDetails extends Component {
         const { classes } = this.props;
         return (
             <Fragment>
-                <Tooltip title="edit details" placement="top">
-                    <IconButton onClick={this.handleOpen} className={classes.button}>
-                        <EditIcon color="primary" />
-                    </IconButton>
-                </Tooltip>
+                <CSButton tip="edit details" onClick={this.handleOpen} btnClassName={classes.button}>
+                    <EditIcon color="primary"/>
+                </CSButton>
                 <Dialog
                     open={this.state.open}
                     onClose={this.handleClose}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import CSButton from '../util/CSButton';
 
 // material-ui islevleri
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 
+//icons
+import AddIcon from '@material-ui/icons/Add';
+import HomeIcon from '@material-ui/icons/Home';
+import Notifications from '@material-ui/icons/Notifications';
 
 class Navbar extends Component {
     render() {
+        const { authenticated } = this.props;
         return (
             <AppBar>
                 <Toolbar className="nav-container">
-                    <Button color="inherit" component={Link} to="/login">Giris</Button>
-                    <Button color="inherit" component={Link} to="/">Anasayfa</Button>
-                    <Button color="inherit" component={Link} to="/signup">Uye ol</Button>
+                    {authenticated ? (
+                        <Fragment>
+                            <CSButton tip="post a scream">
+                                <AddIcon />
+                            </CSButton>
+                            <Link to="/">
+                                <CSButton tip="home">
+                                    <HomeIcon />
+                                </CSButton>
+                            </Link>
+                            <CSButton tip="Notifications">
+                                <Notifications />
+                            </CSButton>
+                        </Fragment>
+                    ) : (
+                            <Fragment>
+                                <Button color="inherit" component={Link} to="/login">Giris</Button>
+                                <Button color="inherit" component={Link} to="/">Anasayfa</Button>
+                                <Button color="inherit" component={Link} to="/signup">Uye ol</Button>
+                            </Fragment>
+                        )}
                 </Toolbar>
             </AppBar>
         );
     }
 }
 
-export default Navbar;
+Navbar.propTypes = {
+    authenticated: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = state => ({
+    authenticated: state.user.authenticated
+})
+
+export default connect(mapStateToProps)(Navbar);
