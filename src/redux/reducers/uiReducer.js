afcc976e867dabe3dd0f3f8c103ad5aae296f2f3
deeppaz <deeppaz@hotmail.com>
2019-11-21 23:49:55 +0300

    fixed loading spinner bug

--- src/redux/reducers/uiReducer.js
+++ src/redux/reducers/uiReducer.js
@@ -10,7 +10,7 @@ export default function(state = initialState, action) {
         case SET_ERRORS:
             return {
                 ...state,
-                loading: true,
+                loading: false,
                 errors: action.payload
             };
         case CLEAR_ERRORS:
