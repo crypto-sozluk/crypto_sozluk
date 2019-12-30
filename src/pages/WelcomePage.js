0a162254fe51272892e61ba932afd2867ff12017
deeppaz <deeppaz@hotmail.com>
2019-12-30 17:50:23 +0300

    update

---
+++ src/pages/WelcomePage.js
@@ -0,0 +1,135 @@
+import React, { Component } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import PropTypes from 'prop-types';
+import LoginLogo from '../images/icon.png';
+import { Link } from 'react-router-dom';
+
+//mui islevleri
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+//redux islevleri
+import { connect } from 'react-redux';
+import { userWelcome } from '../redux/actions/userActions';
+
+const styles = (theme) => ({
+    ...theme.spreadThis
+});
+
+class WelcomePage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            errors: {}
+        }
+    };
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.UI.errors) {
+            this.setState({ errors: nextProps.UI.errors });
+        }
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const userData = {
+            email: this.state.email,
+            password: this.state.password
+        };
+        this.props.userWelcome(userData, this.props.history)
+    };
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+    render() {
+        const {
+            classes,
+            UI: { loading }
+        } = this.props;
+        const { errors } = this.state;
+        return (
+            <Grid container className={classes.form}>
+                <Grid item sm />
+                <Grid item sm>
+                    <img src={LoginLogo} className={classes.image} alt="cryptosozluk logo" />
+                    <Typography variant="h2" className={classes.pageTitle}>
+                        Giriş Yap
+                    </Typography>
+                    <form noValidate onSubmit={this.handleSubmit}>
+                        <TextField
+                            id="email"
+                            name="email"
+                            type="email"
+                            label="Email"
+                            className={classes.TextField}
+                            helperText={errors.email}
+                            error={errors.email ? true : false}
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                            fullWidth
+                        />
+                        <TextField
+                            id="password"
+                            name="password"
+                            type="password"
+                            label="Şifre"
+                            className={classes.TextField}
+                            helperText={errors.password}
+                            error={errors.password ? true : false}
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                            fullWidth
+                        />
+                        {errors.general && (
+                            <Typography variant="body2" className={classes.customError}>
+                                {errors.general}
+                            </Typography>
+                        )}
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            className={classes.button}
+                            disabled={loading}
+                        >
+                            Giriş yap
+                          {loading && (
+                                <CircularProgress size={30} className={classes.progress} />
+                            )}
+                        </Button>
+                        <br />
+                        <br />
+                        <small>hesabınız yok mu? uye olmak için <Link to="/signup">burayı tıklayın</Link></small>
+                    </form>
+                </Grid>
+                <Grid item sm />
+            </Grid>
+        );
+    };
+};
+
+WelcomePage.propTypes = {
+    classes: PropTypes.object.isRequired,
+    userWelcome: PropTypes.func.isRequired,
+    user: PropTypes.object.isRequired,
+    UI: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+    user: state.user,
+    UI: state.UI
+});
+
+const mapActionsToProps = {
+    userWelcome
+}
+
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(WelcomePage));
