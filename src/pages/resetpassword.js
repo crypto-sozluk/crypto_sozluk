a3b1892f1032afd9e3a420bb1f49122e135be703
deeppaz <deeppaz@hotmail.com>
2020-01-07 20:47:06 +0300

    update reset and forgot page

---
+++ src/pages/resetpassword.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class resetpassword extends Component {
+    render() {
+        return (
+            <div>
+                hi i'm reset password page
+            </div>
+        )
+    }
+}
+
+export default resetpassword
