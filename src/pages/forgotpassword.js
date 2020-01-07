ae1aaaa2293a1e05fc60881d137aba9c858b184c
deeppaz <deeppaz@hotmail.com>
2020-01-07 20:46:39 +0300

    added forgot password page

---
+++ src/pages/forgotpassword.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class forgotpassword extends Component {
+    render() {
+        return (
+            <div>
+                hey i'm forgot password page
+            </div>
+        )
+    }
+}
+
+export default forgotpassword
